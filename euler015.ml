283c8fa415752652f49a2bbce7125046ce980caa
Ralph Recto <recto.ralph@gmail.com>
2014-10-12 13:05:33 -0400

    solved #15

---
+++ euler015.ml
@@ -0,0 +1,5 @@
+
+(* no coding. the answer is simply (40 choose 20): we have to make 20 down and
+ * 20 right moves in every path, or 40 total. From these 40, determining the
+ * place of the 20 down moves determines also the place of the 20 right moves.
+ * )
